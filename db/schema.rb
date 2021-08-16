268a58a0aa71835b04738cad65f68c1e6afcd00c
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-08-16 23:29:54 +0300

    refactor: adding username to users

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_08_14_171058) do
 
   create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
