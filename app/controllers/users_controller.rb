9dcd9fe12aa599ea7dcd192a1243458b7bc6f0ba
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-08-18 20:46:07 +0300

    refactor: adding show for users to show user by id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,13 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = User.all
-       if @users
-          render json: {
-          users: @users
-       }
-      else
-          render json: {
-          status: 500,
-          errors: ['no users found']
-      }
-     end
+    users = User.all
+    render json: { users: users }
   end
   
+  def show
+    user = User.find(params[:id])
+    render json: { user: user }
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'private/test'
   get '/current_user', to: 'current_user#index'
-  resources :users, only: [:index] do 
-    resources :items, only: [:index]
+  resources :users, only: [:index, :show] do 
+    resources :items, only: [:index, :show]
  end
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
