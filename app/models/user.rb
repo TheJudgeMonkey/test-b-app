c6243d490d90c6435430ccb89c6a517e378ef733
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-08-19 22:55:29 +0300

    refactor: adding posts model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
-
+  has_many :posts
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
-  validates :username, presence: true
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/current_user', to: 'current_user#index'
   resources :users, only: [:index, :show] do 
     resources :items, only: [:index, :show]
+  end
+  resources :posts, only: [:index, :show] do 
+    resources :items, only: [:index, :show] 
  end
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
